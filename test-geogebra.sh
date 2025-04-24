66e9c24c8bf36deaca9cbaa15c497253fae65337
Zoltán Kovács <zoltan@geogebra.org>
2025-04-24 21:15:44 +0200

    Add first version to test GeoGebra translator automatically

---
+++ test-geogebra.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script tests all *.p files in GeoGebra, after converting them into .ggb files.
+# To avoid annoying error messages in popup windows, run this script like this:
+# xvfb-run -a -s "-screen 0 1024x768x24" ./test-geogebra.sh
+
+trap "echo 'Exit forced'" EXIT
+trap "echo 'Term forced'" TERM
+trap "echo 'Kill forced'" KILL
+
+GEOGEBRA=`which geogebra-discovery`
+TIMEOUT=10
+OUT_DIR=`pwd`/Generated
+
+# Build ADGLibToolkit:
+cd Tools/ADG-Lib_tools
+TOOLKIT_DIR=`pwd`
+make
+
+cd ../../TPTP/PointsOnly/GCLCcollection
+P_DIR=`pwd`
+for i in *.p; do
+  cd "$OUT_DIR"
+  TESTNAME=`basename $i .p`
+  echo -n "$TESTNAME..."
+  # Convert the .p file to geogebra.xml:
+  "$TOOLKIT_DIR/ADGLibToolkit" "$P_DIR/$i" geogebra.xml -geogebra \
+    > "$TESTNAME.adgtoolkit.out" 2> "$TESTNAME.adgtoolkit.err"
+  # Create GeoGebra file:
+  zip -q "$TESTNAME.ggb" "geogebra.xml"
+  # Run GeoGebra:
+  timeout $TIMEOUT $GEOGEBRA --prover=timeout:$TIMEOUT --logFile="$TESTNAME.log" \
+    --regressionFile="$TESTNAME.result" --language=en "$TESTNAME.ggb" \
+    > "$TESTNAME.out" 2> "$TESTNAME.err"
+  grep --silent "Statement is GENERALLY TRUE\|Statement is TRUE" "$TESTNAME.log" && {
+    TIME=`grep "Benchmarking" "$TESTNAME.log" | head -1 | awk '{print $5}'`
+    echo " true ($TIME ms)"
+    } || {
+    echo " unsuccessful"
+    }
+  done
