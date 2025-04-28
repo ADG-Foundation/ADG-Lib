e60f43e88f1f60d9b0bbb7fa849bd48bdd38adc0
Zoltán Kovács <zoltan@geogebra.org>
2025-04-28 21:58:47 +0200

    Remove comments

--- test-geogebra.sh
+++ test-geogebra.sh
@@ -30,9 +30,10 @@ for i in *.p; do
   TESTNAME=`basename $i .p`
   echo -n "$TESTNAME..."
   # Convert the .p file to geogebra.xml:
-  "$TOOLKIT_DIR/ADGLibToolkit" "$P_DIR/$i" geogebra.xml -geogebra \
+  "$TOOLKIT_DIR/ADGLibToolkit" "$P_DIR/$i" geogebra.xml.tmp -geogebra \
     > "$TESTNAME.adgtoolkit.out" 2> "$TESTNAME.adgtoolkit.err"
   # Create GeoGebra file:
+  grep -v ^% geogebra.xml.tmp > geogebra.xml # remove comments
   zip -q "$TESTNAME.ggb" "geogebra.xml"
   # Run GeoGebra:
   timeout $TIMEOUT $GEOGEBRA --prover=timeout:$TIMEOUT --logFile="$TESTNAME.log" \
