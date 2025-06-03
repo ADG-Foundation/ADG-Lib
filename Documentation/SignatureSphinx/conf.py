94ad808edee8b0bdbe997fc084ad6ea314b021b9
Julien Narboux <narboux@irif.fr>
2025-06-03 11:57:31 +0200

    add .sty file use this style file in Sphynx doc

--- Documentation/SignatureSphinx/conf.py
+++ Documentation/SignatureSphinx/conf.py
@@ -14,12 +14,16 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.mathjax']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+# -- Options for LaTeX output ------------------------------------------------
+latex_additional_files = ["../Signature/adgsignature.sty"]
+latex_elements = {
+    'extrapackages': r'\usepackage{adgsignature}'
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
